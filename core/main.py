0ff7c3372fbb2d8810212afdb71994862ba509fc
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-05-05 03:41:29 +0330

    Add form-based Q&A handling and logger integration

--- core/main.py
+++ core/main.py
@@ -1,6 +1,35 @@
 import telebot
 import os
+import logging
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 API_TOKEN = os.environ.get('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    logger.info("triggered welcom")
+    bot.reply_to(message, """\
+Hi there :D
+""")
+    
+@bot.message_handler(commands=['setname'])
+def setup_name(meessage):
+    bot.send_message(meessage.chat.id,"what is your first name brother?")
+    bot.register_next_step_handler(meessage,assign_first_name)
+
+def assign_first_name(message):
+    first_name = message.text
+    bot.send_message(message.chat.id,f'what is ur last name{first_name}?')
+    bot.register_next_step_handler(message,assign_last_name)
+
+def assign_last_name(message,first_name):
+    last_name = message.chat.id
+    bot.send_message(message.chat.id,f'well well welcome {first_name} {last_name} to my bot :)')
+
+
+
+
 
 bot.infinity_polling()
