f2305eea6aa00f6f6d0046fe7f4cd05271457fa9
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-05-28 06:22:42 +0330

    Learned to create a bot that receives photos, compresses them, and sends the compressed images back

--- core/main.py
+++ core/main.py
@@ -1,7 +1,8 @@
 import telebot
 import os
 import logging
-from yt_dlp import YoutubeDL
+import io
+from PIL import Image
 
 API_TOKEN = os.environ.get('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
@@ -17,41 +18,21 @@ telebot.logger.setLevel(logging.INFO)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     logger.info("triggered welcome")
-    bot.send_message(message.chat.id, "Hey send a link and I’ll download it for you :3")
+    bot.send_message(message.chat.id, "Hey send a photo and i will compress it for you :3")
 
-def download_youtube_video(url, download_dir="downloads/"):
-    ydl_opts = {
-        'format': 'mp4',
-        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
-        'quiet': True,
-        'merge_output_format': 'mp4',
-    }
+@bot.message_handler(content_types=['photo'])
+def pic_comp(message):
+    photo = message.photo[-1]
+    file_info = bot.get_file(photo.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
 
-    with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info)
-        title = info.get('title', 'No Title')
-        return filename, title
+    image = Image.open(io.BytesIO(downloaded_file))
 
-@bot.message_handler(func=lambda message: True)
-def handle_url(message):
-    url = message.text.strip()
-    logger.info(f"Received URL: {url}")
+    file_path = os.path.join(DOWNLOAD_DIR, f"{photo.file_id}_compressed.jpg")
 
-    try:
-        if "youtube.com" in url or "youtu.be" in url:
-            file_path, title = download_youtube_video(url)
-            with open(file_path, 'rb') as video:
-                bot.send_document(
-                    chat_id=message.chat.id,
-                    document=video,
-                    caption=title,
-                    reply_parameters=message.id
-                )
-        else:
-            bot.reply_to(message, "Only YouTube links are supported.")
-    except Exception as e:
-        logger.error(f"Error downloading file: {e}")
-        bot.reply_to(message, "Problem downloading the requested file :C")
-
+    image.save(file_path, format="JPEG", optimize=True, quality=30)
+    with open(file_path, 'rb') as photo_file:
+        bot.send_chat_action(chat_id=message.chat.id,action='upload_photo')
+        bot.send_photo(message.chat.id, photo_file, caption="Enjoy 💙")
+    
 bot.infinity_polling()
