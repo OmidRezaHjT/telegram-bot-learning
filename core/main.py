8b734e843980878402ef48f613e97d9975a07060
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-05-30 08:32:19 +0330

    learned how to pin a replied message using the /pin command

--- core/main.py
+++ core/main.py
@@ -7,15 +7,6 @@ bot = telebot.TeleBot(API_TOKEN)
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
-@bot.message_handler(func=lambda message:message.chat.type in ["supergroup","group"])
-def handle_gp(message):
-    logger.info("triggered gp")
-    
-
-@bot.message_handler(func= lambda message:message.chat.type in ["private"])
-def handle_pv(message):
-    logger.info("triggered pv")
-
 @bot.chat_join_request_handler()
 def join_req_handler(request):
     logger.info(request)
@@ -25,5 +16,19 @@ def join_req_handler(request):
 def handle_join_msg(message):
     for member in message.new_chat_members:
         bot.send_message(message.chat.id, f'Welcome to the group, {member.first_name}!')
+
+
+@bot.message_handler(commands=['pin'])
+def pin_msg(message):
+    if message.reply_to_message:
+        try:
+            bot.pin_chat_message(
+                chat_id=message.chat.id,
+                message_id=message.reply_to_message.message_id
+            )
+        except Exception as e:
+            bot.reply_to(message, f"❌ Failed to pin: {e}")
+    else:
+        bot.reply_to(message, "⚠️ You must reply to a message to pin it.")
 
 bot.infinity_polling()
